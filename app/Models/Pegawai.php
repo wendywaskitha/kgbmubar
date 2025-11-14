fa8ccfc688c671013aaaeda6eb586af7fdfe1b40
jatikusuma <wendywas22@gmail.com>
2025-11-14 23:29:11 +0800

    fix: Add SoftDeletes trait to Pegawai model

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Traits\TenantAware;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pegawai extends Model
 {
-    use HasFactory, TenantAware;
+    use HasFactory, SoftDeletes, TenantAware;
 
     /**
      * The table associated with the model.
