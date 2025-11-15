312f12a16b23aabe0d2f47499d8c3d114cf907cc
jatikusuma <wendywas22@gmail.com>
2025-11-15 22:01:14 +0800

    Fix: Enlarge PDF modal preview - 85vh height, responsive full-page view for modal

--- resources/views/filament/components/pdf-preview.blade.php
+++ resources/views/filament/components/pdf-preview.blade.php
@@ -1,10 +1,4 @@
-<div class="flex justify-center items-center p-4">
-    <iframe 
-        src="{{ $url }}" 
-        type="application/pdf"
-        class="w-full h-[70vh] border border-gray-300 rounded-lg"
-        title="PDF Preview"
-    >
-        <p>Browser Anda tidak mendukung tampilan PDF. <a href="{{ $url }}" target="_blank">Klik di sini untuk membuka file.</a></p>
-    </iframe>
+@props(['url'])
+<div style="width:100%;height:85vh;background:#222;padding:1rem;display:flex;justify-content:center;align-items:center;">
+    <iframe src="{{ $url }}" width="100%" height="100%" style="border:0;min-height:550px;max-height:85vh;box-shadow:0 0 30px #0008;" allowfullscreen></iframe>
 </div>
