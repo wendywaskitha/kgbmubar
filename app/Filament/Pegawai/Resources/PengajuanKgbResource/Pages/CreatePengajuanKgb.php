c8cef477fde3131e805f8d77ceb14684a96460f1
jatikusuma <wendywas22@gmail.com>
2025-11-15 09:04:51 +0800

    fix(Pegawai CreatePengajuanKgb): auto fill pegawai_id, user_pengaju_id, jenis_pengajuan. Notifikasi & proteksi UX

--- app/Filament/Pegawai/Resources/PengajuanKgbResource/Pages/CreatePengajuanKgb.php
+++ app/Filament/Pegawai/Resources/PengajuanKgbResource/Pages/CreatePengajuanKgb.php
@@ -6,6 +6,7 @@ use Filament\Actions;
 use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Pegawai\Resources\PengajuanKgbResource;
+use Filament\Notifications\Notification;
 
 class CreatePengajuanKgb extends CreateRecord
 {
@@ -13,8 +14,39 @@ class CreatePengajuanKgb extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // Paksa assign user login
-        $data['user_pengaju_id'] = Auth::id();
+        $user = Auth::user();
+        $pegawai = $user->pegawai;
+        if (!$pegawai) {
+            Notification::make()
+                ->danger()
+                ->title('Tidak Dapat Membuat Pengajuan')
+                ->body('Akun Anda belum terhubung dengan data pegawai. Silakan hubungi admin.')
+                ->persistent()
+                ->send();
+            $this->halt();
+        }
+        $data['user_pengaju_id'] = $user->id;
+        $data['pegawai_id'] = $pegawai->id;
+        // Set jenis_pengajuan = mandiri by default untuk pegawai
+        $data['jenis_pengajuan'] = 'mandiri';
+
+        // Set tanggal_pengajuan jika status diajukan
+        if (isset($data['status']) && $data['status'] === 'diajukan') {
+            $data['tanggal_pengajuan'] = now();
+        }
         return $data;
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Pengajuan KGB Berhasil Dibuat')
+            ->body('Pengajuan KGB Anda telah berhasil disimpan.');
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
