54613b8e58176789891a673efa72c3eea0787d5c
jatikusuma <wendywas22@gmail.com>
2025-11-15 18:54:29 +0800

    feat: add pdf preview partial for Filament modal action

---
+++ resources/views/filament/app/resources/pengajuan-kgb-resource/partials/preview-pdf.blade.php
@@ -0,0 +1,8 @@
+<div class="p-4 text-center">
+    <iframe src="{{ $fileUrl }}" class="w-full h-[600px] rounded shadow border mx-auto" frameborder="0"></iframe>
+    <div class="mt-4">
+        <a href="{{ $fileUrl }}" target="_blank" download class="text-primary-600 hover:underline">
+            Download PDF
+        </a>
+    </div>
+</div>
