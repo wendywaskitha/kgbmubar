9a4c45e11627ce1b1d877190042cc67f60b440a3
jatikusuma <wendywas22@gmail.com>
2025-11-15 18:53:26 +0800

    feat: add image preview partial for Filament modal action

---
+++ resources/views/filament/app/resources/pengajuan-kgb-resource/partials/preview-image.blade.php
@@ -0,0 +1,8 @@
+<div class="p-4 text-center">
+    <img src="{{ $fileUrl }}" alt="{{ $fileName }}" class="inline-block max-w-full max-h-[600px] rounded shadow border mx-auto">
+    <div class="mt-4">
+        <a href="{{ $fileUrl }}" target="_blank" download class="text-primary-600 hover:underline">
+            Download Gambar
+        </a>
+    </div>
+</div>
